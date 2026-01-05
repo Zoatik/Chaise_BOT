55b5b36e378e63f7fd6b7e3971f27eb642d0894d
Zoatik <119761787+Zoatik@users.noreply.github.com>
2026-01-05 16:19:25 +0100

    available movements for all pieces

--- Bots/minmaxSimple.py
+++ Bots/minmaxSimple.py
@@ -36,13 +36,12 @@ def minMaxBot(player_sequence, board, time_budget, **kwargs):
         "mk" : 1,
 
         # Attacks
-        "ptp" : 2,
-        "ptn" : 3,
-        "ptr" : 3,
-        "ptb" : 4,
-        "ptq" : 8,
-        "ptk" : 100,
-
+        "tp" : 4,
+        "tn" : 6,
+        "tr" : 6,
+        "tb" : 8,
+        "tq" : 16,
+        "tk" : 100,
         # Specials
         "pup" : 10
     }
@@ -62,6 +61,14 @@ def minMaxBot(player_sequence, board, time_budget, **kwargs):
             if board[x,y][1]!=color:
                 continue
 
+
+            def isInBounds(x,y):
+                if x < 0 or x > 7:
+                    return False
+                if y < 0 or y > 7:
+                    return False
+                return True
+
             # We now find ourselves on a cell containing one
             # of our pieces.
 
@@ -74,6 +81,7 @@ def minMaxBot(player_sequence, board, time_budget, **kwargs):
                 allMoves = []
                 match board[x,y][0]:
                     case "p":
+                        mp_val = moveValues["mp"]
                         print("////// Pawn //////")
                         # Queen upgrade ?
                         if x+1 == 7:
@@ -84,37 +92,37 @@ def minMaxBot(player_sequence, board, time_budget, **kwargs):
                         else:
                             if board[x+1,y] == "":
                                 # print("PAWN MOVED FORWARDS")
-                                allMoves.append([moveValues["mp"], (x,y), (x+1,y)])
+                                allMoves.append([mp_val, (x,y), (x+1,y)])
                         # Diag attacks ?
-                        print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSCANNNNNNNNNNNNNNNNNNNNNNN")
                         if y + 1 <= 7:
                             if board[x+1,y+1] == "" or board[x+1,y+1] == "X":
                                 pass
                             elif board[x+1,y+1][1] != color:
                                 # print("PAWN ATTACKED")
-                                allMoves.append([moveValues["pt"+board[x+1,y+1][0]], (x,y), (x+1,y+1)])
+                                allMoves.append([moveValues["t"+board[x+1,y+1][0]] + mp_val, (x,y), (x+1,y+1)])
                         if y - 1 >= 0:
                             if board[x+1,y-1] == "" or board[x+1,y-1] == "X":
                                 pass
                             elif board[x+1,y-1][1] != color:
                                 # print("PAWN ATTACKED")
-                                allMoves.append([moveValues["pt"+board[x+1,y-1][0]], (x,y), (x+1,y-1)])
+                                allMoves.append([moveValues["t"+board[x+1,y-1][0]] + mp_val, (x,y), (x+1,y-1)])
                         
                     case "n":
                         print("Knight")
+                        allMoves.extend( getKnightMoves(x,y) )
                         pass
                     case "r":
                         print("Rook")
-                        pass
+                        allMoves.extend( getRookMoves(x,y) )
                     case "b":
                         print("Bishop")
-                        pass
+                        allMoves.extend( getBishopMoves(x,y) )
                     case "q":
                         print("Queen")
-                        pass
+                        allMoves.extend( getQueenMoves(x,y) )
                     case "k":
                         print("King")
-                        pass
+                        allMoves.extend( getKingMoves(x,y) )
                     case _:
                         print("Zis is not on of ours !")
                         sys.exit("Stopping execution.")
@@ -122,6 +130,174 @@ def minMaxBot(player_sequence, board, time_budget, **kwargs):
                 print("All moves", allMoves)
                 return allMoves
             
+
+            def getKnightMoves(x,y):
+                mn_val = moveValues["mn"]
+                knightMoves = []
+                potentialMoves = [
+                    (x+2, y+1),
+                    (x+2, y-1),
+                    (x-2, y+1),
+                    (x-2, y-1),
+                    (x+1, y+2),
+                    (x+1, y-2),
+                    (x-1, y+2),
+                    (x-1, y-2)
+                ]
+                for move in potentialMoves:
+                    if isInBounds(move[0], move[1]):
+                        if board[move[0], move[1]] == "" :
+                            knightMoves.append([mn_val, (x,y), (move[0], move[1])])
+                        elif board[move[0], move[1]] == "X":
+                            pass
+                        elif board[move[0], move[1]][1] != color:
+                            knightMoves.append([moveValues["t"+board[move[0], move[1]][0]] + mn_val, (x,y), (move[0], move[1])])
+                return knightMoves
+            
+            def getRookMoves(x,y, isQueen=False):
+                rookMoves = []
+                mr_val = moveValues["mr"] if not isQueen else moveValues["mq"]
+                # Up
+                for i in range(x+1, 8):
+                    if board[i,y] == "":
+                        rookMoves.append([mr_val, (x,y), (i,y)])
+                    elif board[i,y] == "X":
+                        break
+                    elif board[i,y][1] != color:
+                        rookMoves.append([moveValues["t" + board[i,y][0]] + mr_val, (x,y), (i,y)])
+                        break
+                    else:
+                        break
+                # Down
+                for i in range(x-1, -1, -1):
+                    if board[i,y] == "":
+                        rookMoves.append([mr_val, (x,y), (i,y)])
+                    elif board[i,y] == "X":
+                        break
+                    elif board[i,y][1] != color:
+                        rookMoves.append([moveValues["t" + board[i,y][0]] + mr_val, (x,y), (i,y)])
+                        break
+                    else:
+                        break
+                # Right
+                for j in range(y+1, 8):
+                    if board[x,j] == "":
+                        rookMoves.append([mr_val, (x,y), (x,j)])
+                    elif board[x,j] == "X":
+                        break
+                    elif board[x,j][1] != color:
+                        rookMoves.append([moveValues["t" + board[x,j][0]] + mr_val, (x,y), (x,j)])
+                        break
+                    else:
+                        break
+                # Left
+                for j in range(y-1, -1, -1):
+                    if board[x,j] == "":
+                        rookMoves.append([mr_val, (x,y), (x,j)])
+                    elif board[x,j] == "X":
+                        break
+                    elif board[x,j][1] != color:
+                        rookMoves.append([moveValues["t" + board[x,j][0]] + mr_val, (x,y), (x,j)])
+                        break
+                    else:
+                        break
+                return rookMoves
+            
+            def getBishopMoves(x,y, isQueen=False):
+                mb_val = moveValues["mb"] if not isQueen else moveValues["mq"]
+                bishopMoves = [] 
+                # Up-Right
+                i, j = x+1, y+1
+                while isInBounds(i,j):
+                    if board[i,j] == "":
+                        bishopMoves.append([mb_val, (x,y), (i,j)])
+                    elif board[i,j] == "X":
+                        break
+                    elif board[i,j][1] != color:
+                        bishopMoves.append([moveValues["t" + board[i,j][0]] + mb_val, (x,y), (i,j)])
+                        break
+                    else:
+                        break
+                    i += 1
+                    j += 1
+                # Up-Left
+                i, j = x+1, y-1
+                while isInBounds(i,j):
+                    if board[i,j] == "":
+                        bishopMoves.append([mb_val, (x,y), (i,j)])
+                    elif board[i,j] == "X":
+                        break
+                    elif board[i,j][1] != color:
+                        bishopMoves.append([moveValues["t" + board[i,j][0]] + mb_val, (x,y), (i,j)])
+                        break
+                    else:
+                        break
+                    i += 1
+                    j -= 1
+                # Down-Right
+                i, j = x-1, y+1
+                while isInBounds(i,j):
+                    if board[i,j] == "":
+                        bishopMoves.append([mb_val, (x,y), (i,j)])
+                    elif board[i,j] == "X":
+                        break
+                    elif board[i,j][1] != color:
+                        bishopMoves.append([moveValues["t" + board[i,j][0]] + mb_val, (x,y), (i,j)])
+                        break
+                    else:
+                        break
+                    i -= 1
+                    j += 1
+                # Down-Left
+                i, j = x-1, y-1
+                while isInBounds(i,j):
+                    if board[i,j] == "":
+                        bishopMoves.append([mb_val, (x,y), (i,j)])
+                    elif board[i,j] == "X":
+                        break
+                    elif board[i,j][1] != color:
+                        bishopMoves.append([moveValues["t" + board[i,j][0]] + mb_val, (x,y), (i,j)])
+                        break
+                    else:
+                        break
+                    i -= 1
+                    j -= 1
+                return bishopMoves
+            
+            def getQueenMoves(x,y):
+                queenMoves = []
+                # Combine rook and bishop moves
+                # Rook-like moves
+                queenMoves.extend(getRookMoves(x,y, isQueen=True))
+                # Bishop-like moves
+                queenMoves.extend(getBishopMoves(x,y, isQueen=True))
+                return queenMoves
+            
+            def getKingMoves(x,y):
+                mk_val = moveValues["mk"]
+                kingMoves = []
+                potentialMoves = [
+                    (x+1, y),
+                    (x-1, y),
+                    (x, y+1),
+                    (x, y-1),
+                    (x+1, y+1),
+                    (x+1, y-1),
+                    (x-1, y+1),
+                    (x-1, y-1)
+                ]
+                for move in potentialMoves:
+                    if isInBounds(move[0], move[1]):
+                        if board[move[0], move[1]] == "" :
+                            kingMoves.append([mk_val, (x,y), (move[0], move[1])])
+                        elif board[move[0], move[1]] == "X":
+                            pass
+                        elif board[move[0], move[1]][1] != color:
+                            kingMoves.append([moveValues["t"+board[move[0], move[1]][0]] + mk_val, (x,y), (move[0], move[1])])
+                return kingMoves
+            
+
+
             allMoves = getAllMoves()
 
             if len(allMoves) != 0:
